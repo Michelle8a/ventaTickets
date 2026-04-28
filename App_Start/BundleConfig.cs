170770e3858f70c4b769725a3849ab0435b7b7a4
Michelle <miche8a@gmail.com>
2026-04-27 23:36:57 -0600

    Avance: Presentaciones y modificacion del Layout con un sidebar

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -23,8 +23,9 @@ namespace GestionTickets
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                        "~/Content/bootstrap.css",
+                        "~/Content/Site.css",
+                        "~/Content/Layout.css"));
         }
     }
 }
---
+++ Controllers/PresentacionesController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GestionTickets.Controllers
+{
+    public class PresentacionesController : Controller
+    {
+        private gestion_ticketsEntities db = new gestion_ticketsEntities();
+
+        // GET: Presentaciones
+        public ActionResult Index()
+        {
+            var presentaciones = db.sp_mostrar_presentaciones(null).ToList();
+            return View(presentaciones);
+        }
+
+        // GET: Presentaciones/Crear
+        public ActionResult Crear()
+        {
+            ViewBag.Eventos = db.sp_mostrar_eventos(null).ToList();
+            ViewBag.Artistas = db.sp_mostrar_artistas(null).ToList();
+            return View();
+        }
+
+        // POST: Presentaciones/Crear
+        [HttpPost]
+        public ActionResult Crear(int id_evento, int id_artista, DateTime fecha_hora_inicio,
+            string escenario, DateTime? fecha_hora_fin, int? orden)
+        {
+            db.sp_agregar_presentacion(id_evento, id_artista, fecha_hora_inicio,
+                escenario, fecha_hora_fin, orden);
+            return RedirectToAction("Index");
+        }
+
+        // GET: Presentaciones/Editar/5
+        public ActionResult Editar(int id)
+        {
+            var presentacion = db.sp_mostrar_presentaciones(null)
+                .FirstOrDefault(p => p.id_presentacion == id);
+            if (presentacion == null) return HttpNotFound();
+            ViewBag.Artistas = db.sp_mostrar_artistas(null).ToList();
+            return View(presentacion);
+        }
+
+        // POST: Presentaciones/Editar/5
+        [HttpPost]
+        public ActionResult Editar(int id_presentacion, int id_artista, string escenario,
+            DateTime? fecha_hora_inicio, DateTime? fecha_hora_fin, int? orden)
+        {
+            db.sp_actualizar_presentacion(id_presentacion, id_artista, escenario,
+                fecha_hora_inicio, fecha_hora_fin, orden);
+            return RedirectToAction("Index");
+        }
+
+        // POST: Presentaciones/Eliminar/5
+        [HttpPost]
+        public ActionResult Eliminar(int id)
+        {
+            db.sp_eliminar_presentacion(id);
+            return RedirectToAction("Index");
+        }
+
+        // POST: Presentaciones/Activar/5
+        [HttpPost]
+        public ActionResult Activar(int id)
+        {
+            db.sp_activar_presentacion(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
