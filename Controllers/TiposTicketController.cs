f7bc3f75f1cede79f684dfa1676b5b5891eaab34
Diego280371 <dm2803717@gmail.com>
2026-04-29 16:35:54 -0600

    Resolviendo problemitas

---
+++ Controllers/TiposTicketController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GestionTickets.Controllers
+{
+    public class TiposTicketController : Controller
+    {
+        // GET: TiposTicket
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
